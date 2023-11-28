d025b24fb4c98254231d4c897e4313840d57693a
ikhebgeenaccount <lars.abbo@gmail.com>
2023-11-28 14:35:11 +0100

    Added easy way to find cosmological parameters for a particular cosmol id

---
+++ analysis/cosmologies.py
@@ -0,0 +1,24 @@
+import re
+import os
+import pandas as pd
+
+def read_cosmologies_info():
+	file = os.path.join('data', 'cosmologies.csv')
+
+	df = pd.read_csv(file, delimiter='\t', skiprows=1, names=['id', 'Omega_m', 'S_8', 'h', 'w_0', 'sigma_8', 'Omega_cdm'])
+
+	return df
+
+cosmologies_info = read_cosmologies_info()
+
+def get_cosmological_parameters(cosmology_id):
+	if isinstance(cosmology_id, int) or cosmology_id == 'fid':
+		return cosmologies_info[cosmologies_info['id'] == cosmology_id]
+	else:
+		patt = re.compile('.*Cosmol([0-9]+|fid)')
+
+		if mat := patt.match(cosmology_id):
+			return cosmologies_info[cosmologies_info['id'] == mat]
+		else:
+			print(f'Assuming {cosmology_id} is fiducial cosmology')
+			return cosmologies_info[cosmologies_info['id'] == 'fid']
