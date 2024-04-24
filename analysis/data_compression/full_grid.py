f80d4e896b287846daad64cb24ed0464d32e2ca2
ikhebgeenaccount <lars.abbo@gmail.com>
2024-04-24 15:23:06 +0200

    Updated to CosmologyData

--- analysis/data_compression/full_grid.py
+++ analysis/data_compression/full_grid.py
@@ -2,13 +2,14 @@ from typing import List
 
 import numpy as np
 
+from analysis.cosmology_data import CosmologyData
 from analysis.data_compression.index_compressor import IndexCompressor
 from analysis.persistence_diagram import PersistenceDiagram
 
 
 class FullGrid(IndexCompressor):
 
-	def __init__(self, cosmoslics_pds: List[PersistenceDiagram], slics_pds: List[PersistenceDiagram]):
+	def __init__(self, cosmoslics_datas: List[CosmologyData], slics_data: List[CosmologyData]):
 		# Build set of indices that contain all entries
-		indices = np.indices((2, 100, 100))
-		super().__init__(cosmoslics_pds, slics_pds, indices=indices.T)
+		indices = np.indices((len(slics_data[0].zbins_pds) * 2, 100, 100))
+		super().__init__(cosmoslics_datas, slics_data, indices=indices.T)
