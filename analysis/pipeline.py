794b4bb8a42f3c730c87abd49093b6ac7cfc2b52
ikhebgeenaccount <lars.abbo@gmail.com>
2024-05-03 16:50:23 +0200

    Removed is_notebook

--- analysis/pipeline.py
+++ analysis/pipeline.py
@@ -8,19 +8,7 @@ from analysis.cosmology_data import CosmologyData
 from analysis.map import Map
 from analysis.persistence_diagram import BettiNumbersGrid, PersistenceDiagram
 from analysis.persistence_diagram import BettiNumbersGridVarianceMap, PixelDistinguishingPowerMap
-
-
-def is_notebook() -> bool:
-    try:
-        shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
-            return True   # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False      # Probably standard Python interpreter
+from utils.is_notebook import is_notebook
 
 if is_notebook():
 	from tqdm.notebook import tqdm
