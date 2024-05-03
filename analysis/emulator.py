421d0017b3d0ed4f7935df60f5b97785c91c212a
ikhebgeenaccount <lars.abbo@gmail.com>
2024-05-03 14:35:38 +0200

    Removed all StandardScaler behaviour

--- analysis/emulator.py
+++ analysis/emulator.py
@@ -6,7 +6,6 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.preprocessing import StandardScaler
 
 from analysis.data_compression.compressor import Compressor
 from analysis import cosmologies
@@ -25,14 +24,9 @@ class Emulator:
 		self.regressor_args = regressor_args
 		self.regressor = regressor_type(**regressor_args)
 
-		self.standard_scaler = StandardScaler()
-		self.standard_scaler_y = StandardScaler()
 		# Ensure that training_set contains numpy.ndarrays
 		self.training_set['input'] = np.array(self.training_set['input'])
-		self.training_set['scaled_input'] = self.standard_scaler.fit_transform(self.training_set['input'])
 		self.training_set['target'] = np.array(self.training_set['target'])
-		self.training_set['scaled_target'] = self.standard_scaler_y.fit_transform(self.training_set['target'])
-		# self.training_set['scaled_target'] = self.standard_scaler.fit_transform(self.training_set['target'])
 
 		self.data_vector_length = self.training_set['target'].shape[1]
 
@@ -42,8 +36,6 @@ class Emulator:
 		self.regressor.fit(self.training_set['input'], self.training_set['target'])
 	
 	def predict(self, X):
-		# scale X through self.standard_scaler, not fitting again
-		# print(self.regressor.predict(X, return_std=True))
 		return self.regressor.predict(X)
 	
 	def validate(self, make_plot=False):
@@ -51,13 +43,13 @@ class Emulator:
 
 		all_mse = []
 
-		for i, (train_index, test_index) in enumerate(loo.split(self.training_set['scaled_input'])):
+		for i, (train_index, test_index) in enumerate(loo.split(self.training_set['input'])):
 			temp_regr = self.regressor_type(**self.regressor_args)
 			temp_regr.fit(self.training_set['input'][train_index], self.training_set['target'][train_index])
 
 			# Not np.abs to also get negative error
 			mse = (self.training_set['target'][test_index][0] - temp_regr.predict(self.training_set['input'][test_index])[0]) / self.training_set['target'][test_index][0]
-			# mse = np.square((self.training_set['target'][test_index][0] - self.regressor.predict(self.training_set['scaled_input'][test_index])[0]) / self.training_set['target'][test_index][0])
+			# mse = np.square((self.training_set['target'][test_index][0] - self.regressor.predict(self.training_set['input'][test_index])[0]) / self.training_set['target'][test_index][0])
 
 			all_mse.append(mse)
 
@@ -209,26 +201,25 @@ class PerFeatureEmulator(Emulator):
 		pass
 
 	def predict(self, X):
-		scaled_X = self.standard_scaler.transform(X)
 		# TODO: fix for multiple predictions in X at once (multiple input vectors in X)
-		return np.array([[regressor.fit(scaled_X).flatten()[0] for regressor in self.regressors]])
+		return np.array([[regressor.fit(X).flatten()[0] for regressor in self.regressors]])
 
 	def validate(self, make_plot=False):
 		loo = LeaveOneOut()
 
 		all_mse = []
 
-		for i, (train_index, test_index) in enumerate(loo.split(self.training_set['scaled_input'])):
+		for i, (train_index, test_index) in enumerate(loo.split(self.training_set['input'])):
 			mse = []
 			for j, regr in enumerate(self.regressors):
-				train_input = self.training_set['scaled_input'][train_index]
+				train_input = self.training_set['input'][train_index]
 				train_target = self.training_set['target'][train_index][:, j]
-				test_input = self.training_set['scaled_input'][test_index]
+				test_input = self.training_set['input'][test_index]
 				test_target = self.training_set['target'][test_index][:, j]
 				regr.fit(train_input, train_target)
 
 				mse.append((test_target - regr.predict(test_input)[0]) / test_target)
-				# mse = np.square((self.training_set['target'][test_index][0] - self.regressor.predict(self.training_set['scaled_input'][test_index])[0]) / self.training_set['target'][test_index][0])
+				# mse = np.square((self.training_set['target'][test_index][0] - self.regressor.predict(self.training_set['input'][test_index])[0]) / self.training_set['target'][test_index][0])
 
 			mse = np.array(mse).flatten()
 			all_mse.append(mse)
